3da080652993e2c3aa70a851de1f0dca0de10868
Andres Olivares <spartan_abg@hotmail.com>
2024-10-04 14:07:08 -0500

    added nav guard

--- src/router.js
+++ src/router.js
@@ -5,18 +5,30 @@ import CardDetails from './components/CardDetails.vue'
 import LoginPage from './components/LoginPage.vue'
 import SettingsPage from './components/SettingsPage.vue'
 
+import { useAuth } from './composables/useAuth'
+const {isAuthenticated} = useAuth()
+
 const routes = [
   { path: '/', name: 'Home', component: HomePage },
   { path: '/other', name: 'Other', component: () => import('@/views/OtherPage.vue') },
   { path: '/employees/:id', name: 'CardDetails', component: CardDetails },
   { path: '/login', name: 'LoginPage', component: LoginPage },
-  { path: '/Settings', name: 'SettingsPage', component: SettingsPage },
+  { path: '/Settings', name: 'SettingsPage', component: SettingsPage, meta: {requiresAuth: true} },
 
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+})
+
+//navigation guard
+router.beforeEach((to, _, next) => {
+  if (to.meta.requiresAuth && !isAuthenticated.value) {
+    next({name: 'LoginPage', query: {redirect: to.fullPath}})
+  } else {
+    next()
+  }
 })
 
 export default router
